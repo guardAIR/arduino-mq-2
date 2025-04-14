a93f70a3932ec453e084612785c3428c2a22b643
Bruno Oliveira <bruno.doliveira@sptech.school>
2025-04-14 19:10:17 -0300

    Update mq2.ino

--- mq2.ino
+++ mq2.ino
@@ -4,20 +4,18 @@ const int VALOR_MINIMO=100;//Valor mínimo de captação
 const int VALOR_MAXIMO=1000;//Valor máximo da captação
 
 void setup() {//Função que será executada
-  Serial.begin(9600); //Inicia a comunicação a 9600 bauds
-  while (!Serial);
-  Serial.println("%");
+    Serial.begin(9600); //Inicia a comunicação a 9600 bauds
 }
 
 void loop() {//Função que será executada continuamente
-  int valorSensor= analogRead(PINO_SENSOR_MQ2);//A porta analógica do sensor
-  float porcentagem=((float)(valorSensor - VALOR_MINIMO) / (VALOR_MAXIMO - VALOR_MINIMO))*100;//Transforma em porcentagem
+    int valorSensor= analogRead(PINO_SENSOR_MQ2);//A porta analógica do sensor
+    float porcentagem=((float)(valorSensor - VALOR_MINIMO) / (VALOR_MAXIMO - VALOR_MINIMO))*100;//Transforma em porcentagem
     if(porcentagem < 0){//Se o sensor estiver negativo
       porcentagem = 0;//Não está funcionando
     }else if (porcentagem > 100){//Se for 1
       porcentagem = 100;//Está ligado
     }
-Serial.println(porcentagem);
-
-delay(500);// Tempo entre os resultados
+    Serial.println(porcentagem);
+    
+    delay(500);// Tempo entre os resultados
 }
