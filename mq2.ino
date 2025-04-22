f33b067d6e2bcb020ea177d07b15bad8d4b451d4
Ally Awada <ally.awada@sptech.school>
2025-04-22 09:57:28 -0300

    Novo código para o arduino

--- mq2.ino
+++ mq2.ino
@@ -1,21 +1,20 @@
-const int PINO_SENSOR_MQ2 =A0; //Saída do sensor na A0
+int sensorPin = A5;
+float ppm;
 
-const int VALOR_MINIMO=100;//Valor mínimo de captação
-const int VALOR_MAXIMO=1000;//Valor máximo da captação
-
-void setup() {//Função que será executada
-    Serial.begin(9600); //Inicia a comunicação a 9600 bauds
+void setup() {
+  Serial.begin(9600);
 }
 
-void loop() {//Função que será executada continuamente
-    int valorSensor= analogRead(PINO_SENSOR_MQ2);//A porta analógica do sensor
-    float porcentagem=((float)(valorSensor - VALOR_MINIMO) / (VALOR_MAXIMO - VALOR_MINIMO))*100;//Transforma em porcentagem
-    if(porcentagem < 0){//Se o sensor estiver negativo
-      porcentagem = 0;//Não está funcionando
-    }else if (porcentagem > 100){//Se for 1
-      porcentagem = 100;//Está ligado
-    }
-    Serial.println(porcentagem);
-    
-    delay(500);// Tempo entre os resultados
+void loop() {
+  int leitura = analogRead(sensorPin);
+  if (leitura > 40){
+    leitura = leitura - 40;
+  }
+  else {
+    leitura = 0;
+  }
+  ppm = (leitura / 1023.0) * 100;
+  Serial.println(ppm);
+
+  delay(1000);
 }
